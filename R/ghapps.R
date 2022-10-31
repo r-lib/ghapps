fb95a4f7df46cb81851ede5a57874b047394b201
Jeroen Ooms <jeroenooms@gmail.com>
2022-11-01 00:04:51 +0100

    Overhaul API and docs to make more suitable for public use cc https://github.com/jeroen/ghapps/issues/1

--- R/ghapps.R
+++ R/ghapps.R
@@ -1,36 +1,79 @@
-#' Authenticate as an App
+#' Authenticate as a GitHub App
 #'
-#' Authenticates with the GitHub API on behalf of a GitHub app using the
-#' app-id and private-key file that you get when registering the app. The
-#' result is a temporary token that is valid for 1 hour, which you can use
-#' just like other tokens to act on behalf of the app using e.g. [gh()].
+#' GitHub apps provide a powerful way to manage fine grained programmatic access
+#' to specific git repositories, without having to create dummy users, and which
+#' are safer than PATs for automated tasks. This package extends [gh] to let you
+#' authenticate and interact with the GitHub API in R on behalf of an app.
+#'
+#' GitHub has two modes of authentication: on behalf of a user, or on behalf of a
+#' "GitHub app". An app is a first class actor within GitHub. This means it has
+#' its own permissions to specific repositories, without being tied to any user
+#' account. In fact, you can think of an app as a special type of dummy user.
+#'
+#' ## Creating a GitHub app
 #'
 #' To register a new app go to:
-#' [https://github.com/settings/apps/new](https://github.com/settings/apps/new)
+#' [https://github.com/settings/apps/new](https://github.com/settings/apps/new).
+#' You can register as many apps as you like, with different permissions, for
+#' different purposes.
+#'
+#' You can choose if the the app is public (to allow others to give the app access
+#' to their repo by "installing" the app) or if the app is only for your own
+#' account. The latter is basically a safe way to provide limited access to a
+#' single repository, without exposing your user account in any way.
+#'
+#' ## Acting on behalf of an app in R
+#'
+#' We can interact with most of the GitHub API on behalf of an app in exactly the
+#' same way as a regular user. The only difference is that instead of a personal
+#' access token (PAT) we need to generate a temporary app token, which
+#' looks very similar. From here we  can use [gh()] in the same way as usual.
+#'
+#' Authentication on behalf of an app is a two step process. First you need to
+#' generate a so-called JWT with [gh_app_jwt()] using the App-ID and a RSA key file
+#' that you can retrieve on GitHub in the app settings. This JWT is only valid for
+#' 5 minutes, and is used to generate app tokens with [gh_app_token()].
+#' This app token works the same as a PAT, but it is valid for 1 hour and has
+#' permission only to the repositories that you specified as the `installations`
+#' parameter in [gh_app_token()]. From here you can use [gh()] to perform all the
+#' operations on the git repositories that the app has permissions for.
+#'
+#' ## Using this in CI
+#'
+#' You can also specify the app-id and private key via environment variables
+#' `GH_APP_ID` and `GH_APP_KEY`. The latter can simply contain the verbatim content
+#' of the private key file that you expose as a 'secret' in the CI. This way you
+#' can authenticate on behalf of a github-app inside a github action, to safely
+#' automate sensitive operations.
 #'
 #' @export
-#' @rdname gh_apps
+#' @name ghapps
+#' @rdname ghapps
+#' @param jwt a jwt credential object generated with [gh_app_jwt()]
+gh_app_info <- function(jwt = gh_app_jwt()){
+  gh_as_app('/app', jwt = jwt)
+}
+
+#' @export
+#' @rdname ghapps
 #' @importFrom gh gh
-#' @name GitHub Apps
-#' @param installation the target app-installation to authenticate with, This must
-#' either be a user / organization name such as `"ropensci"`, or a repository that
-#' has the app installed for example `"ropensci/magick"`.
-#' @param app_id a string with the github app id
-#' @param app_key file or string with your private key, passed to [openssl::read_key]
+#' @param installation the target repositorie(s) which we want to access.
+#' Either a user / organization name such as `"ropensci"`, or a specific repository
+#' that has the app installed for example `"ropensci/magick"`.
 #' @return a temporary token that will be valid for 1 hour
-#' @examples \donttest{
-#' Sys.setenv('GH_APP_ID' = 87942)
-#' Sys.setenv('GH_APP_KEY' = "~/Downloads/r-universe.2020-11-25.private-key.pem")
-#' gh_app_installation_count()
-#' gh_app_installation_list()
-#' gh_app_installation_info('jeroen')
-#' gh_app_token('jeroen')
+#' @examples
+#' # Authenitcate and show some metadata about our demo-app
+#' gh_app_info(jwt = gh_demo_jwt())
+#' gh_app_installation_list(jwt = gh_demo_jwt())
+#' \donttest{
+#' # This requires that user 'testingjerry' has our app installed:
+#' gh_app_installation_info('testingjerry', jwt = gh_demo_jwt())
+#' token <- gh_app_token('testingjerry', jwt = gh_demo_jwt())
+#'
+#' # Use the token in gh() to do things on behalf of the app
+#' res <- gh::gh('/users/testingjerry', .token = token)
 #' }
-gh_app_token <- function(installation, app_id = Sys.getenv('GH_APP_ID'),
-                         app_key = Sys.getenv('GH_APP_KEY')){
-  if(!nchar(app_id) || !nchar(app_key))
-    stop("No app_id or app_key found")
-  jwt <- gh_app_jwt(app_id = app_id, app_key = app_key)
+gh_app_token <- function(installation, jwt = gh_app_jwt()){
   endpoint <- if(grepl("/", installation)){
     sprintf('/repos/%s/installation', installation)
   } else {
@@ -42,31 +85,20 @@ gh_app_token <- function(installation, app_id = Sys.getenv('GH_APP_ID'),
 }
 
 #' @export
-#' @rdname gh_apps
-gh_app_installation_count <- function(app_id = Sys.getenv('GH_APP_ID'), app_key = Sys.getenv('GH_APP_KEY')){
-  if(!nchar(app_id) || !nchar(app_key))
-    stop("No app_id or app_key found")
-  jwt <- gh_app_jwt(app_id = app_id, app_key = app_key)
-  gh_as_app('/app', jwt = jwt)$installations_count
+#' @rdname ghapps
+gh_app_installation_count <- function(jwt = gh_app_jwt()){
+  gh_app_info(jwt = jwt)$installations_count
 }
 
 #' @export
-#' @rdname gh_apps
-gh_app_installation_list <- function(app_id = Sys.getenv('GH_APP_ID'),
-                                     app_key = Sys.getenv('GH_APP_KEY')){
-  if(!nchar(app_id) || !nchar(app_key))
-    stop("No app_id or app_key found")
-  jwt <- gh_app_jwt(app_id = app_id, app_key = app_key)
+#' @rdname ghapps
+gh_app_installation_list <- function(jwt = gh_app_jwt()){
   gh_as_app('/app/installations', jwt = jwt, per_page = 100, .limit = 1e6)
 }
 
 #' @export
-#' @rdname gh_apps
-gh_app_installation_info <- function(installation, app_id = Sys.getenv('GH_APP_ID'),
-                                     app_key = Sys.getenv('GH_APP_KEY')){
-  if(!nchar(app_id) || !nchar(app_key))
-    stop("No app_id or app_key found")
-  jwt <- gh_app_jwt(app_id = app_id, app_key = app_key)
+#' @rdname ghapps
+gh_app_installation_info <- function(installation, jwt = gh_app_jwt()){
   endpoint <- if(grepl("/", installation)){
     sprintf('/repos/%s/installation', installation)
   } else {
@@ -76,21 +108,11 @@ gh_app_installation_info <- function(installation, app_id = Sys.getenv('GH_APP_I
 }
 
 #' @export
-#' @rdname gh_apps
-gh_app_installation_delete <- function(installation, app_id = Sys.getenv('GH_APP_ID'),
-                                     app_key = Sys.getenv('GH_APP_KEY')){
-  installation_info <- gh_app_installation_info(installation = installation,
-                                              app_id = app_id, app_key = app_key)
+#' @rdname ghapps
+gh_app_installation_delete <- function(installation, jwt = gh_app_jwt()){
+  installation_info <- gh_app_installation_info(installation = installation, jwt = jwt)
   endpoint <- paste0('/app/installations/', installation_info$id)
-  jwt <- gh_app_jwt(app_id = app_id, app_key = app_key)
   gh_as_app(endpoint, jwt = jwt, .method = 'DELETE')
-}
-
-#' @export
-#' @rdname gh_apps
-#' @param name slug of the github app from `https://github.com/apps/{name}`
-gh_app_info <- function(name = 'r-universe'){
-  gh::gh(paste0('/apps/', name))
 }
 
 gh_as_app <- function(endpoint, jwt, ...){
@@ -98,7 +120,36 @@ gh_as_app <- function(endpoint, jwt, ...){
   gh(endpoint, ..., .token = "", .send_headers = jwt_auth)
 }
 
-gh_app_jwt <- function(app_id, app_key){
-  payload <- jose::jwt_claim(exp = unclass(Sys.time()) + 300, iss = app_id)
-  jose::jwt_encode_sig(payload, app_key)
+#' @export
+#' @rdname ghapps
+#' @param name slug of the github app from `https://github.com/apps/{name}`
+gh_app_public_info <- function(name = 'r-universe'){
+  gh::gh(paste0('/apps/', name))
+}
+
+#' @export
+#' @rdname ghapps
+#' @param app_id a string with the github app id
+#' @param app_key file path or string with your private key, passed to [openssl::read_key].
+#' or a key object returned by [openssl::read_key]
+gh_app_jwt <- function(app_id = Sys.getenv('GH_APP_ID'), app_key = Sys.getenv('GH_APP_KEY')){
+  if(is_empty(app_id))
+    stop("No app_id is set")
+  if(is_empty(app_key))
+    stop("No app_key is set")
+  expires <- Sys.time() + 300
+  payload <- jose::jwt_claim(exp = unclass(expires), iss = app_id)
+  structure(jose::jwt_encode_sig(payload, app_key), app_id = app_id, expires = expires)
+}
+
+#' @export
+#' @rdname ghapps
+gh_demo_jwt <- function(){
+  keyfile <- system.file('keys/demo-app-for-r.2022-10-31.private-key.pem', package = 'ghapps')
+  app_key <- openssl::read_key(keyfile, password = 'test')
+  gh_app_jwt(app_id = '256536', app_key = app_key)
+}
+
+is_empty <- function(x){
+  !length(x) || (is.character(x) && !nchar(x))
 }
